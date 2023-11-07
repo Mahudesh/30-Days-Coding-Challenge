bd2af1f12040103facc9705a53e82b8e93acbf70
Mahudesh <115523760+Mahudesh@users.noreply.github.com>
2023-11-07 20:49:54 +0530

    Create LeetCode_Apply Operations To An Array.cpp

---
+++ Day 6/LeetCode_Apply Operations To An Array.cpp
@@ -0,0 +1,42 @@
+Problem :
+
+You are given a 0-indexed array nums of size n consisting of non-negative integers.
+
+You need to apply n - 1 operations to this array where, in the ith operation (0-indexed), you will apply the following on the ith element of nums:
+
+If nums[i] == nums[i + 1], then multiply nums[i] by 2 and set nums[i + 1] to 0. Otherwise, you skip this operation.
+After performing all the operations, shift all the 0's to the end of the array.
+
+For example, the array [1,0,2,0,0,1] after shifting all its 0's to the end, is [1,2,1,0,0,0].
+Return the resulting array.
+
+Input:
+
+class Solution {
+public:
+    vector<int> applyOperations(vector<int>& nums)
+     {
+         for(int i=0;i<nums.size()-1;i++)
+         {
+             if(nums[i]==nums[i+1])
+             {
+                 nums[i]=2*(nums[i]);
+                 nums[i+1]=0;
+             }
+         }
+         vector<int>temp;
+         for(int i=0;i<nums.size();i++)
+         {
+             if(nums[i]!=0)
+             {
+                 temp.push_back(nums[i]);
+             }
+         }
+         int newlen=nums.size()-temp.size();
+         for(int i=0;i<newlen;i++)
+         {
+             temp.push_back(0);
+         }
+         return temp;
+    }
+};
