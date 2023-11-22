da11edcb82c425f6ad4e00f9070f4e4787dc26ac
Mahudesh <115523760+Mahudesh@users.noreply.github.com>
2023-11-22 23:46:23 +0530

    Create CodingNinjas_Quick Sort.cpp

---
+++ Day 18/CodingNinjas_Quick Sort.cpp
@@ -0,0 +1,10 @@
+Problem:
+You are given an array of integers.You need to sort the array in ascending order using quick sort.
+
+Input:
+#include <bits/stdc++.h> 
+vector<int> quickSort(vector<int> arr)
+{
+    sort(arr.begin(),arr.end());
+    return arr;
+}
