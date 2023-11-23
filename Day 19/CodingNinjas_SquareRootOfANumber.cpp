171f7094f6c54dd874072047fb70eab8cceb4b0a
Mahudesh <115523760+Mahudesh@users.noreply.github.com>
2023-11-23 21:47:38 +0530

    Create CodingNinjas_SquareRootOfANumber.cpp

---
+++ Day 19/CodingNinjas_SquareRootOfANumber.cpp
@@ -0,0 +1,10 @@
+Problem:
+You are given a positive integer 'n'.
+Your task is to find and return its square root. If 'n' is not a perfect square, then return the floor value
+of sqrt(n).
+
+Input:
+int floorSqrt(int n)
+{
+    return floor(sqrt(n));
+}
