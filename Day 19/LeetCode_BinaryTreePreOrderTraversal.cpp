e5feb7c40ec2243136ed545b618993f0a35fded5
Mahudesh <115523760+Mahudesh@users.noreply.github.com>
2023-11-23 21:45:20 +0530

    Create LeetCode_BinaryTreePreOrderTraversal.cpp

---
+++ Day 19/LeetCode_BinaryTreePreOrderTraversal.cpp
@@ -0,0 +1,33 @@
+Problem:
+Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
+Input:
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+void preorder(TreeNode* root, vector<int> &arr)
+{
+        if(root==NULL)
+        return;
+        arr.push_back(root->val);
+        preorder(root->left,arr);
+        preorder(root->right,arr);
+}
+    vector<int> preorderTraversal(TreeNode* root) 
+    {
+        vector<int>arr;
+        preorder(root,arr);
+        
+        return arr;
+    }
+};
