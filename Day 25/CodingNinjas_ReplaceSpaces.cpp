a95c34858dac1cf4d95fa8235f24de62531eb2f5
Mahudesh <115523760+Mahudesh@users.noreply.github.com>
2023-12-02 22:54:26 +0530

    Create CodingNinjas_ReplaceSpaces.cpp

---
+++ Day 25/CodingNinjas_ReplaceSpaces.cpp
@@ -0,0 +1,14 @@
+Problem:
+Replace Spaces
+
+Input:
+#include <bits/stdc++.h> 
+string replaceSpaces(string &str)
+{
+	for(int i=0;i<str.length();i++)
+	{
+		if(str[i]==' ')
+		str.replace(i,1,"@40");
+	}
+	return str;
+}
