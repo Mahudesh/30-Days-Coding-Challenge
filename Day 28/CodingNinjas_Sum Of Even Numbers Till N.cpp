a9905c7b08aeb5fd6a980174735079360170678c
Mahudesh <115523760+Mahudesh@users.noreply.github.com>
2023-12-08 20:31:15 +0530

    Create CodingNinjas_Sum Of Even Numbers Till N.cpp

---
+++ Day 28/CodingNinjas_Sum Of Even Numbers Till N.cpp
@@ -0,0 +1,14 @@
+Problem:
+ Sum Of Even Numbers Till N
+
+Input:
+#include <bits/stdc++.h> 
+long long evenSumTillN(int n) 
+{
+   long long sum=0;
+    for(int i=2;i<=n;i=i+2)
+    {
+        sum=sum+i;
+    }
+    return sum;
+}
